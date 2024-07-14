fd4914974675cbd727de0545fe0b8e3ebf0862a9
MOONtyzoo <91515108+MOONtyzoo@users.noreply.github.com>
2024-07-14 08:15:06 -0700

    Add generateGrid function base
    Uses the number input to generate a NxN grid. For now nothing is
    actually added to the DOM, but an alert is given to show that
    the parameter is being passed in properly.

--- javascript.js
+++ javascript.js
@@ -5,11 +5,20 @@ let gridSizeInput = buttonBar.querySelector("#gridSizeInput")
 let generateButton = buttonBar.querySelector("#generateButton")
 let resetButton = buttonBar.querySelector("#resetButton")
 
-generateButton.addEventListener("click", () => alert("generate!"))
+generateButton.addEventListener("click", () => {
+    let gridSize = gridSizeInput.value
+    gridSizeInput.value = ""
+    generateGrid(gridSize)
+})
 resetButton.addEventListener("click", () => clearGrid())
 
 function clearGrid() {
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild)
     }
+}
+
+function generateGrid(size) {
+    clearGrid()
+    alert(`Generating a ${size}x${size} grid`)
 }
