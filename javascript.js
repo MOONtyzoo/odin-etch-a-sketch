eee5e7d60d5dab9b1ee218691d31f5db0b6ffb85
MOONtyzoo <91515108+MOONtyzoo@users.noreply.github.com>
2024-07-14 07:59:14 -0700

    Add clearGrid function
    Used by the reset button to clear the grid contents.
    Another function will replace those contents with new
    elements.

--- javascript.js
+++ javascript.js
@@ -6,4 +6,10 @@ let generateButton = buttonBar.querySelector("#generateButton")
 let resetButton = buttonBar.querySelector("#resetButton")
 
 generateButton.addEventListener("click", () => alert("generate!"))
-resetButton.addEventListener("click", () => alert("reset!"))
+resetButton.addEventListener("click", () => clearGrid())
+
+function clearGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild)
+    }
+}
